c1e922157dcb47e5e0e29372f3571a2fab2aa941
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2024-09-05 01:08:44 +0300

    Caching - Backend

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Last-In First-Out caching module.
+"""Most Recently Used caching module.
 """
 from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class MRUCache(BaseCaching):
     """Represents an object that allows storing and
-    retrieving items from a dictionary with a LIFO
+    retrieving items from a dictionary with an MRU
     removal mechanism when the limit is reached.
     """
     def __init__(self):
@@ -24,12 +24,15 @@ class LIFOCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Retrieves an item by key.
         """
-        return self.cache_data.get(key, None)
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
